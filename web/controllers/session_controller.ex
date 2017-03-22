47b78f340d0e3d631057f50cdd3bd44364e26d0a
Mingding, Han <hanmd82@gmail.com>
2017-03-22 18:58:21 +0800

    Clear user session in auth module and implement 'delete' action in SessionController

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -17,4 +17,10 @@ defmodule HelloPhoenix.SessionController do
         |> render("new.html")
     end
   end
+
+  def delete(conn, _) do
+    conn
+    |> HelloPhoenix.Auth.logout()
+    |> redirect(to: page_path(conn, :index))
+  end
 end
